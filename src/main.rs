61f01cc5393928efcac64adf2e0eb778ec830c12
Xiaonuo Gantan <xgantan@execvision.io>
2023-10-12 10:25:57 -0400

    Initial: Read a file from poem.txt

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Cannot read the file");
+    println!("With text:\n{contents}");
+}
